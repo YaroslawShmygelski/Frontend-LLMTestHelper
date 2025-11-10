87cd18f716e0a91272de2bcd3db3c1e4a3c8d5e2
Yaroslav <yaroslawsh04@gmail.com>
2025-11-10 03:26:34 +0100

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(1)
 
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card text-4xl p-64 text-green-600" >
         <button onClick={() => setCount((count) => count *2)}>
           count is {count}
         </button>
@@ -16,4 +16,3 @@ function App() {
   )
 }
 
-export default App
